fd2b73394daf9fb51d0793fc633be87eab85909f
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-05-04 10:54:21 +0000

    Update storage JS

--- src/module/loaddata.js
+++ src/module/loaddata.js
@@ -1,20 +1,16 @@
-let listStore = JSON.parse(localStorage.getItem('taskList')) || [];
+import { taskStorage } from "./newdata";
 
 const bkList = document.querySelector('.listTodo');
 
-function updateData() {
-  listStore = JSON.parse(localStorage.getItem('taskList'));
-}
-
-export const displayList = () => {
-  bkList.innerHTML = ``;
-  updateData();
-  for (let i = 0; i < listStore.length; i += 1) {
+let displayList;
+export default displayList = () => {
+  bkList.innerHTML = '';
+  for (let i = 0; i < taskStorage.length; i += 1) {
     bkList.innerHTML += `
     <div class='align-row'>
       <div class='label-checkbox'>
         <input type="checkbox" id="check" name="check" >
-        <label class="label" id=${listStore[i].keys} for="check">${listStore[i].description}</label>
+        <label class="label" id=${taskStorage[i].keys} for="check">${taskStorage[i].description}</label>
       </div>
      <div>
         <span class="delete" id=${i}><i class="fa fa-trash" aria-hidden="true"></i></span>
@@ -27,11 +23,10 @@ export const displayList = () => {
 
   const deleteBook = () => {
     const remoVingBtnsEl = [...document.getElementsByClassName('delete')];
-    console.log(remoVingBtnsEl);
     remoVingBtnsEl.forEach((item) => {
       item.addEventListener('click', (e) => {
-        listStore.splice(e.target.id, 1);
-        localStorage.setItem('taskList', JSON.stringify(listStore));
+        taskStorage.splice(e.target.id, 1);
+        localStorage.setItem('taskList', JSON.stringify(taskStorage));
         displayList();
       });
     });
