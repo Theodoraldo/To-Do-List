5f2006eb6b9e47b3090c291727163ca2c92d8cb5
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-05-04 10:53:24 +0000

    Update module JS file

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import './style.css';
-import NewTasks from './module/newdata';
-import { displayList } from './module/loaddata';
+import NewTasks from './module/newdata.js';
+import displayList from './module/loaddata.js';
 
 const aNew = document.getElementById('editTask');
 aNew.addEventListener('keypress', (e) => {
-  if (e.key === "Enter") {
+  if (e.key === 'Enter') {
     e.preventDefault();
     const addNwTodo = new NewTasks(aNew.value);
     addNwTodo.addTask();
+    aNew.value = '';
     displayList();
-    aNew.value = "";
   }
 });
 
 const refresh = document.getElementById('refresh');
-
 refresh.addEventListener('click', () => {
   displayList();
 });
